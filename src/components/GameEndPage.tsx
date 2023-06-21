32f5a797920c8f1879c205d5876f0738b12351a7
poomiputh <poomiputh_b@cmu.ac.th>
2023-06-21 23:02:33 +0700

    U. BE

--- src/components/GameEndPage.tsx
+++ src/components/GameEndPage.tsx
@@ -1,11 +1,51 @@
 import { useLocation } from 'react-router-dom'
+import { ProjectName } from '../App'
+import { IScoreRequest, addAppScore, getAllAppScores } from '../api/scoreApi'
+import { useEffect, useState } from 'react'
+import ScoreLists from './ScorePage/ScoreLists'
 
 const GameEndPage = () => {
     const location = useLocation()
+    const [scoresList, setScoresList] = useState<IScoreRequest[]>([])
+    const appScore = {
+        name: location.state.playerName,
+        score: location.state.score,
+        projectName: ProjectName,
+    }
+
+    console.log(appScore)
+
+    const onLoaded = async () => {
+        try {
+            await addAppScore(appScore)
+        } catch (error) {
+            console.log('addAppScore Error')
+        }
+    }
+
+    const handleDeleteSuccess = (deleteScoreID: number) => {
+        setScoresList((prev) => {
+            return prev.filter((score) => score.id !== deleteScoreID)
+        })
+    }
+
+    useEffect(() => {
+        onLoaded()
+        getAllAppScores(ProjectName).then((result) => {
+            setScoresList(result!)
+        })
+    }, [])
+
+    useEffect(() => {
+        console.log(scoresList)
+    }, [scoresList])
+
     return (
         <>
-            <h1>{location.state.playerName}</h1>
-            <h1>{location.state.score}</h1>
+            <ScoreLists
+                scoreLists={scoresList}
+                onDeleteSuccess={handleDeleteSuccess}
+            ></ScoreLists>
         </>
     )
 }

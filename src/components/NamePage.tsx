e9942d2ec76ae8f7a85550de957b41c3ec1376a8
poomiputh <poomiputh_b@cmu.ac.th>
2023-06-24 01:20:35 +0700

    U.

--- src/components/NamePage.tsx
+++ src/components/NamePage.tsx
@@ -1,12 +1,24 @@
 import { useEffect, useState } from 'react'
 import { Button, Grid } from '@mui/material'
 import GamePage from './GamePage'
-import { ConfirmName } from './image_components/ImageComponents'
+import { BackBtn, ConfirmName } from './image_components/ImageComponents'
 
 const NamePage = () => {
     const [startContrast, setConfrimContrast] = useState<string>('100%')
     const [playerName, setPlayerName] = useState<string>('Player')
     const [isStartGame, setIsStartGame] = useState<boolean>(false)
+
+    const [backContrast, setBackContrast] = useState<string>('100%')
+
+    const goToBack = () => {
+        window.location.reload()
+    }
+    const handleBackEnter = () => {
+        setBackContrast('150%')
+    }
+    const handleBackLeave = () => {
+        setBackContrast('100%')
+    }
 
     const handleConfrimEnter = () => {
         setConfrimContrast('150%')
@@ -35,17 +47,38 @@ const NamePage = () => {
                 <>
                     <Grid
                         container
+                        spacing={2}
+                        paddingTop={'2vh'}
+                        marginTop={0}
+                        style={{ position: 'relative', zIndex: '2' }}
+                    >
+                        <Grid item>
+                            <Button
+                                style={{
+                                    backgroundColor: 'transparent',
+                                    filter: 'contrast(' + backContrast + ')',
+                                }}
+                                onMouseEnter={handleBackEnter}
+                                onMouseLeave={handleBackLeave}
+                                onClick={goToBack}
+                            >
+                                <BackBtn></BackBtn>
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        container
                         justifyContent={'center'}
                         spacing={2}
                         marginTop={0}
                         style={{
                             position: 'fixed',
                             zIndex: '2',
-                            top: '5vh',
+                            top: '15vh',
                         }}
                     >
                         <Grid item>
-                            <div style={{ maxWidth: '70vw', fontSize: '2vh' }}>
+                            <div style={{ maxWidth: '60vw', fontSize: '2vh' }}>
                                 How To Play <br />
                                 <br />
                                 1. Random sequence of cards will be revealed at
@@ -68,7 +101,7 @@ const NamePage = () => {
                         style={{
                             position: 'fixed',
                             zIndex: '2',
-                            top: '30vh',
+                            top: '40vh',
                         }}
                     >
                         <Grid item style={{ paddingTop: '4vh' }}>
